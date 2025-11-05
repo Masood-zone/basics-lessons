61d4dc7297cc7f383adbb6e02beebe15b013ac64
SAMMY <samuelawusi17@email.com>
2025-11-05 01:05:19 +0000

    my assignment

---
+++ my-assignment.js
@@ -0,0 +1,32 @@
+// variable - let, const, var
+let houseNumber = 644;
+
+  var roomOne;
+  roomOne = 66;
+ const luckyNumber = 7;
+ //string
+let housePaint = "Green"
+//My concatenation try
+let studentName = "Samuel Awusi"
+let schoolName = " is a student of University of Ghana"
+let  levelName = " in level 300"
+let myIdentity = studentName +  schoolName  + levelName
+//Concatenation-  concatenate a string with my name
+let greeStudentName = "Samuel Awusi"
+// 4 operations
+let operation = "===,++,--,+>,-<,>,<"
+houseNumber++
+houseNumber--
+//boolean - True and False 
+let landLord = houseNumber === luckyNumber
+let sammyAwusi= houseNumber > roomOne
+
+let 2ndName = excel;
+let goodNumber = 66;
+console.log(goodNumber)
+
+
+
+
+
+
