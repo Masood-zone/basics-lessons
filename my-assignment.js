3f4501b8fd5fcdb5dbda74fbfa14d3b7d118bada
masood <masoodacheampong@gmail.com>
2025-11-05 16:48:26 +0000

    Refactor variable declarations and formatting in my-assignment.js

--- my-assignment.js
+++ my-assignment.js
@@ -1,32 +1,26 @@
 // variable - let, const, var
 let houseNumber = 644;
 
-  var roomOne;
-  roomOne = 66;
- const luckyNumber = 7;
- //string
-let housePaint = "Green"
+var roomOne;
+roomOne = 66;
+const luckyNumber = 7;
+//string
+let housePaint = "Green";
 //My concatenation try
-let studentName = "Samuel Awusi"
-let schoolName = " is a student of University of Ghana"
-let  levelName = " in level 300"
-let myIdentity = studentName +  schoolName  + levelName
+let studentName = "Samuel Awusi";
+let schoolName = " is a student of University of Ghana";
+let levelName = " in level 300";
+let myIdentity = studentName + schoolName + levelName;
 //Concatenation-  concatenate a string with my name
-let greeStudentName = "Samuel Awusi"
+let greeStudentName = "Samuel Awusi";
 // 4 operations
-let operation = "===,++,--,+>,-<,>,<"
-houseNumber++
-houseNumber--
-//boolean - True and False 
-let landLord = houseNumber === luckyNumber
-let sammyAwusi= houseNumber > roomOne
+let operation = "===,++,--,+>,-<,>,<";
+houseNumber++;
+houseNumber--;
+//boolean - True and False
+let landLord = houseNumber === luckyNumber;
+let sammyAwusi = houseNumber > roomOne;
 
-let 2ndName = excel;
-let goodNumber = 66;
-console.log(goodNumber)
-
-
-
-
-
-
+// let 2ndName = excel;
+let goodNumber = 86;
+console.log(goodNumber);
