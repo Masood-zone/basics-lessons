d87133650ef3c55755409428fda8246e81eb5cbc
masood <masoodacheampong@gmail.com>
2025-11-26 10:36:02 +0000

    Refactor price filtering and tax calculation logic for clarity

--- Challenges-1.js
+++ Challenges-1.js
@@ -9,6 +9,20 @@ Tasks:
 3. Log the final array.
 */
 const prices = [20, -5, 0, 15, -2, 40, 10];
+// 1. Filter out invalid prices
+const validPrices = prices.filter((price) => price > 0);
+/*
+    Called the prices array =
+    Picked 20, is 20 > 0 = Yes, keep element
+    Picked -5, is -5 > 0 = No, remove element
+    Picked 0, is 0 > 0 = No, remove element
+    Picked 40, is 40 > 0 = Yes, keep element
+    Picked 10, is 10 > 0 = Yes, keep element
+*/
+// 2. Use map to add 10% tax to each valid price.
+// Original price = 100% and Tax = 10% => 100% + 10% = 110% ~ 1.1
+const pricesWithTax = validPrices.map((price) => price * 1.1);
+console.log(pricesWithTax);
 
 /*
 Challenge 2 — Extract usernames
