bb6ad1f82704db86b9f07df1fe8a10d780fb8381
masood <masoodacheampong@gmail.com>
2025-11-26 11:17:03 +0000

    Implement challenge to filter and format even numbers

--- Challenges-5.js
+++ Challenges-5.js
@@ -9,3 +9,10 @@ Tasks:
 3. Print the final array.
 */
 const numbers = [3, 10, 7, 12, 9, 4, 15, 2];
+// 1. Filter all even numbers.
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// 2. Map them into this format: "Even number: 10"
+const displayEvenNumbers = evenNumbers.map(
+  (number) => `Even number: ${number}`
+);
+console.log(displayEvenNumbers);
