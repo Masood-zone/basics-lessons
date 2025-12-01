78166ee1bf89baa5dc2f55b1770943927b4dc21e
masood <masoodacheampong@gmail.com>
2025-12-01 13:21:57 +0000

    Implement user profile building function with helper functions for formatting, age calculation, and adult status

--- Functions(unsolved)/challenge5.js
+++ Functions(unsolved)/challenge5.js
@@ -19,3 +19,42 @@ Skills Practiced
 
 Hint - Call each helper function once, store the results, and return one object.
 */
+
+function format(name) {
+  const firstLetter = name.charAt(0).toUpperCase();
+  const remainingLetters = name.slice(1).toLowerCase();
+  return firstLetter + remainingLetters;
+}
+
+function calculateAge(dateOfBirth) {
+  const date = 2025;
+  const userAge = date - dateOfBirth;
+  return userAge;
+}
+
+function isAdult(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getInitials(fullName) {}
+
+function buildUserProfile(name, yearOfBirth) {
+  const formattedName = format(name);
+  const userAge = calculateAge(yearOfBirth);
+  const userAdult = isAdult(userAge);
+  const initials = getInitials(name);
+
+  return {
+    name: formattedName,
+    age: userAge,
+    isAdult: userAdult,
+    userInitials: initials,
+  };
+}
+
+const user = buildUserProfile("Samuel", 2000);
+console.log(user);
